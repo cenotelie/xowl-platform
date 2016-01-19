67432db1b9dde88a051ca265d64c503aa4f2be96
Laurent Wouters <lwouters@cenotelie.fr>
2016-01-19 18:58:57 +0100

    [feature] Adding consistency management service

--- service-consistency/src/main/java/org/xowl/platform/services/consistency/impl/XOWLConsistencyService.java
+++ service-consistency/src/main/java/org/xowl/platform/services/consistency/impl/XOWLConsistencyService.java
@@ -24,20 +24,19 @@ import org.xowl.platform.kernel.KernelSchema;
 import org.xowl.platform.kernel.ServiceUtils;
 import org.xowl.platform.services.consistency.ConsistencyRule;
 import org.xowl.platform.services.consistency.ConsistencyService;
-import org.xowl.platform.services.consistency.Inconsistency;
 import org.xowl.platform.services.lts.TripleStore;
 import org.xowl.platform.services.lts.TripleStoreService;
+import org.xowl.platform.utils.Utils;
 import org.xowl.store.IOUtils;
 import org.xowl.store.IRIs;
+import org.xowl.store.Vocabulary;
 import org.xowl.store.rdf.IRINode;
 import org.xowl.store.rdf.LiteralNode;
 import org.xowl.store.rdf.QuerySolution;
 import org.xowl.store.sparql.Result;
 import org.xowl.store.sparql.ResultFailure;
 import org.xowl.store.sparql.ResultSolutions;
-import org.xowl.store.xsp.XSPReply;
-import org.xowl.store.xsp.XSPReplyFailure;
-import org.xowl.store.xsp.XSPReplyResultCollection;
+import org.xowl.store.xsp.*;
 
 import java.net.HttpURLConnection;
 import java.util.ArrayList;
@@ -85,14 +84,14 @@ public class XOWLConsistencyService implements ConsistencyService {
 
     /**
      * The timeout for cache invalidation, in nano-seconds
-     * Set to 1 second (1 billion nano-seconds)
+     * Set to 2 second (1 billion nano-seconds)
      */
-    private static final long CACHE_INVALIDATION_TIMEOUT = 1000000000L;
+    private static final long CACHE_INVALIDATION_TIMEOUT = 2000000000L;
 
     /**
      * The current consistency rules
      */
-    private final Collection<ConsistencyRule> rules;
+    private final Collection<XOWLConsistencyRule> rules;
     /**
      * Timestamp of the last time the rules were updated
      */
@@ -100,7 +99,7 @@ public class XOWLConsistencyService implements ConsistencyService {
     /**
      * The current inconsistencies
      */
-    private final Collection<Inconsistency> inconsistencies;
+    private final Collection<XOWLInconsistency> inconsistencies;
     /**
      * Time stamp of the last time the inconsistencies were updated
      */
@@ -213,7 +212,9 @@ public class XOWLConsistencyService implements ConsistencyService {
             String inconsistencyId = ((IRINode) solution.get("i")).getIRIValue();
             String inconsistencyRuleId = ((LiteralNode) solution.get("r")).getLexicalValue();
             String inconsistencyMessage = ((LiteralNode) solution.get("m")).getLexicalValue();
-            inconsistencies.add(new XOWLInconsistency(inconsistencyId, inconsistencyMessage, null, null));
+            XSPReply reply = getRule(inconsistencyId);
+            if (reply.isSuccess())
+                inconsistencies.add(new XOWLInconsistency(inconsistencyRuleId, inconsistencyMessage, ((XSPReplyResult<XOWLConsistencyRule>) reply).getData(), null));
         }
         inconsistenciesTimestamp = System.nanoTime();
         return new XSPReplyResultCollection<>(inconsistencies);
@@ -221,42 +222,145 @@ public class XOWLConsistencyService implements ConsistencyService {
 
     @Override
     public XSPReply getRule(String identifier) {
+        long now = System.nanoTime();
+        long elapsed = now - rulesTimestamp;
+        if (elapsed < CACHE_INVALIDATION_TIMEOUT) {
+            for (XOWLConsistencyRule rule : rules) {
+                if (rule.getIdentifier().equals(identifier))
+                    return new XSPReplyResult<>(rule);
+            }
+            return new XSPReplyFailure("Not found");
+        }
 
-        return null;
+        TripleStoreService lts = ServiceUtils.getService(TripleStoreService.class);
+        if (lts == null)
+            return new XSPReplyFailure("Failed to retrieve the LTS service");
+        TripleStore live = lts.getLiveStore();
+        if (live == null)
+            return new XSPReplyFailure("Failed to resolve the live store");
+        XSPReply reply = live.execute("LIST RULES");
+        if (!reply.isSuccess())
+            return reply;
+        Collection<String> ruleNames = ((XSPReplyResultCollection<String>) reply).getData();
+        reply = live.execute("LIST ACTIVE RULES");
+        if (!reply.isSuccess())
+            return reply;
+        Collection<String> activeRuleNames = ((XSPReplyResultCollection<String>) reply).getData();
+        Result result = live.sparql("SELECT DISTINCT ?r ?n ?d WHERE { GRAPH <" +
+                IOUtils.escapeAbsoluteURIW3C(IRI_RULE_METADATA) +
+                "> { ?r a <" +
+                IOUtils.escapeAbsoluteURIW3C(IRI_RULE) +
+                "> . ?r <" +
+                IOUtils.escapeAbsoluteURIW3C(KernelSchema.NAME) +
+                "> ?n . ?r <" +
+                IOUtils.escapeAbsoluteURIW3C(IRI_DEFINITION) +
+                "> ?d } }");
+        if (!result.isSuccess())
+            return new XSPReplyFailure(((ResultFailure) result).getMessage());
+        ResultSolutions solutions = (ResultSolutions) result;
+        XOWLConsistencyRule target = null;
+        rules.clear();
+        for (QuerySolution solution : solutions.getSolutions()) {
+            String ruleId = ((IRINode) solution.get("r")).getIRIValue();
+            String ruleName = ((LiteralNode) solution.get("n")).getLexicalValue();
+            String ruleDefinition = ((LiteralNode) solution.get("d")).getLexicalValue();
+            if (!ruleNames.contains(ruleId))
+                continue;
+            XOWLConsistencyRule rule = new XOWLConsistencyRule(ruleId, ruleName, activeRuleNames.contains(ruleId), ruleDefinition);
+            if (ruleId.equals(identifier))
+                target = rule;
+            rules.add(rule);
+        }
+        rulesTimestamp = System.nanoTime();
+        if (target == null)
+            return new XSPReplyFailure("Not found");
+        return new XSPReplyResult<>(target);
     }
 
     @Override
     public XSPReply createRule(String name, String message, String prefixes, String conditions) {
-        return null;
+        String id = IRI_RULE_BASE + "#" + Utils.encode(name);
+        String definition = prefixes + " rule <" + IOUtils.escapeAbsoluteURIW3C(id) + "> distinct {\n" + conditions + "\n} => {\n" +
+                "?e <" + IOUtils.escapeAbsoluteURIW3C(Vocabulary.rdfType) + "> <" + IOUtils.escapeAbsoluteURIW3C(IRI_INCONSISTENCY) + ">\n" +
+                "?e <" + IOUtils.escapeAbsoluteURIW3C(IRI_MESSAGE) + "> \"" + IOUtils.escapeStringW3C(message) + "\"\n" +
+                "?e <" + IOUtils.escapeAbsoluteURIW3C(IRI_DEFINITION) + "> <" + IOUtils.escapeAbsoluteURIW3C(id) + ">\n" +
+                "}";
+        TripleStoreService lts = ServiceUtils.getService(TripleStoreService.class);
+        if (lts == null)
+            return new XSPReplyFailure("Failed to retrieve the LTS service");
+        TripleStore live = lts.getLiveStore();
+        if (live == null)
+            return new XSPReplyFailure("Failed to resolve the live store");
+        XSPReply reply = live.execute("ADD RULE " + definition);
+        if (!reply.isSuccess())
+            return reply;
+        Result result = live.sparql("INSERT DATA { GRAPH <" + IOUtils.escapeAbsoluteURIW3C(IRI_RULE_METADATA) + "> {" +
+                "<" + IOUtils.escapeAbsoluteURIW3C(id) + "> <" + IOUtils.escapeAbsoluteURIW3C(Vocabulary.rdfType) + "> <" + IOUtils.escapeAbsoluteURIW3C(IRI_RULE) + "> ." +
+                "<" + IOUtils.escapeAbsoluteURIW3C(id) + "> <" + IOUtils.escapeAbsoluteURIW3C(KernelSchema.NAME) + "> \"" + IOUtils.escapeStringW3C(name) + "\" ." +
+                "<" + IOUtils.escapeAbsoluteURIW3C(id) + "> <" + IOUtils.escapeAbsoluteURIW3C(IRI_DEFINITION) + "> \"" + IOUtils.escapeStringW3C(definition) + "\" ." +
+                "} }");
+        if (!result.isSuccess())
+            return new XSPReplyFailure(((ResultFailure) result).getMessage());
+        XOWLConsistencyRule rule = new XOWLConsistencyRule(id, name, false, definition);
+        rules.add(rule);
+        return new XSPReplyResult<>(rule);
     }
 
     @Override
     public XSPReply activateRule(String identifier) {
-        return null;
+        TripleStoreService lts = ServiceUtils.getService(TripleStoreService.class);
+        if (lts == null)
+            return new XSPReplyFailure("Failed to retrieve the LTS service");
+        TripleStore live = lts.getLiveStore();
+        if (live == null)
+            return new XSPReplyFailure("Failed to resolve the live store");
+        return live.execute("ACTIVATE " + identifier);
     }
 
     @Override
     public XSPReply activateRule(ConsistencyRule rule) {
-        return null;
+        return activateRule(rule.getIdentifier());
     }
 
     @Override
     public XSPReply deactivateRule(String identifier) {
-        return null;
+        TripleStoreService lts = ServiceUtils.getService(TripleStoreService.class);
+        if (lts == null)
+            return new XSPReplyFailure("Failed to retrieve the LTS service");
+        TripleStore live = lts.getLiveStore();
+        if (live == null)
+            return new XSPReplyFailure("Failed to resolve the live store");
+        return live.execute("DEACTIVATE " + identifier);
     }
 
     @Override
     public XSPReply deactivateRule(ConsistencyRule rule) {
-        return null;
+        return deactivateRule(rule.getIdentifier());
     }
 
     @Override
     public XSPReply deleteRule(String identifier) {
-        return null;
+        TripleStoreService lts = ServiceUtils.getService(TripleStoreService.class);
+        if (lts == null)
+            return new XSPReplyFailure("Failed to retrieve the LTS service");
+        TripleStore live = lts.getLiveStore();
+        if (live == null)
+            return new XSPReplyFailure("Failed to resolve the live store");
+        XSPReply reply = live.execute("REMOVE RULE " + identifier);
+        if (!reply.isSuccess())
+            return reply;
+        Result result = live.sparql("DELETE WHERE { GRAPH <" +
+                IOUtils.escapeAbsoluteURIW3C(IRI_RULE_METADATA) +
+                "> { <" +
+                IOUtils.escapeAbsoluteURIW3C(identifier) +
+                "> ?p ?o } }");
+        if (!result.isSuccess())
+            return new XSPReplyFailure(((ResultFailure) result).getMessage());
+        return XSPReplySuccess.instance();
     }
 
     @Override
     public XSPReply deleteRule(ConsistencyRule rule) {
-        return null;
+        return deleteRule(rule.getIdentifier());
     }
 }

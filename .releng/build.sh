1f9de5b8a8104b645ed09ac2e96c709ea34512a5
Laurent Wouters <lwouters@cenotelie.fr>
2016-01-22 20:19:38 +0100

    [releng] Include statistics service into the Docker image build

--- .releng/build.sh
+++ .releng/build.sh
@@ -20,6 +20,7 @@ cp "$ROOT/services/domain/target/xowl-service-domain-$VERSION.jar" "$RELENG/dock
 cp "$ROOT/services/executor/target/xowl-service-executor-$VERSION.jar" "$RELENG/docker/"
 cp "$ROOT/services/httpapi/target/xowl-service-httpapi-$VERSION.jar" "$RELENG/docker/"
 cp "$ROOT/services/lts/target/xowl-service-lts-$VERSION.jar" "$RELENG/docker/"
+cp "$ROOT/services/statistics/target/xowl-service-statistics-$VERSION.jar" "$RELENG/docker/"
 cp "$ROOT/services/webapp/target/xowl-service-webapp-$VERSION.jar" "$RELENG/docker/"
 cp "$ROOT/services/workflow/target/xowl-service-workflow-$VERSION.jar" "$RELENG/docker/"
 

f8d95547ffe1846974b73e5f28d07e0118e54a29
Laurent Wouters <lwouters@cenotelie.fr>
2016-02-08 21:31:47 +0100

    [fix] Adding configuration elements for Activiti

--- .releng/activiti/build.sh
+++ .releng/activiti/build.sh
@@ -6,7 +6,7 @@ DIR="$(dirname "$SCRIPT")"
 # Build
 VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -f "$DIR/pom.xml" -Dexpression=project.version|grep -Ev '(^\[|Download\w+:)')
 echo "Building artifacts for version $VERSION"
-mvn install -f "$DIR/pom.xml"
+mvn clean package -f "$DIR/pom.xml"
 
 # Build the docker image
 docker build -t "xowl/xowl-activiti:$VERSION" "$DIR"

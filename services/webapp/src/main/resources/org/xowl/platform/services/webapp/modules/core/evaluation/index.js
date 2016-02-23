2d2eb2e6cbce236c99d2e5ac0f36fce797aa7ecf
Laurent Wouters <lwouters@cenotelie.fr>
2016-02-23 13:30:57 +0100

    [fix] Adding new assets to the web app

--- services/webapp/src/main/resources/org/xowl/platform/services/webapp/modules/core/evaluation/index.js
+++ services/webapp/src/main/resources/org/xowl/platform/services/webapp/modules/core/evaluation/index.js
@@ -26,10 +26,16 @@ function renderEvaluations(evaluations) {
 function renderEvaluation(index, evaluation) {
 	var cell1 = document.createElement("td");
 	cell1.appendChild(document.createTextNode((index + 1).toString()));
+	var icon = document.createElement("img");
+	icon.src = "/web/assets/evaluation.svg";
+	icon.width = 40;
+	icon.height = 40;
+	icon.style.marginRight = "20px";
 	var link = document.createElement("a");
 	link.href = "eval.html?id=" + encodeURIComponent(evaluation.id);
 	link.appendChild(document.createTextNode(evaluation.name));
 	var cell2 = document.createElement("td");
+	cell2.appendChild(icon);
 	cell2.appendChild(link);
 	var row = document.createElement("tr");
 	row.appendChild(cell1);

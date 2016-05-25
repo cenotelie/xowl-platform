6ba17bf0c0fec0f1c88c98b50d1eb880906e7a3d
Laurent Wouters <lwouters@cenotelie.fr>
2016-05-25 18:34:57 +0200

    [releng] Added script to deploy OSGi bundle for the platform to a location

---
+++ .releng/deploy-bundles.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+SCRIPT="$(readlink -f "$0")"
+RELENG="$(dirname "$SCRIPT")"
+ROOT="$(dirname "$RELENG")"
+
+VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -f "$ROOT/pom.xml" -Dexpression=project.version|grep -Ev '(^\[|Download\w+:)')
+
+cp "$ROOT/kernel/target/dependency/redist-"* "$1"
+cp "$ROOT/kernel/target/dependency/xowl-"* "$1"
+cp "$ROOT/kernel/target/dependency/shiro-"* "$1"
+cp "$ROOT/kernel/target/dependency/slf4j-"* "$1"
+cp "$ROOT/kernel/target/xowl-kernel-$VERSION.jar" "$1"
+cp "$ROOT/services/connection/target/xowl-service-connection-$VERSION.jar" "$1"
+cp "$ROOT/services/consistency/target/xowl-service-consistency-$VERSION.jar" "$1"
+cp "$ROOT/services/evaluation/target/xowl-service-evaluation-$VERSION.jar" "$1"
+cp "$ROOT/services/executor/target/xowl-service-executor-$VERSION.jar" "$1"
+cp "$ROOT/services/httpapi/target/xowl-service-httpapi-$VERSION.jar" "$1"
+cp "$ROOT/services/impact/target/xowl-service-impact-$VERSION.jar" "$1"
+cp "$ROOT/services/lts/target/xowl-service-lts-$VERSION.jar" "$1"
+cp "$ROOT/services/statistics/target/xowl-service-statistics-$VERSION.jar" "$1"
+cp "$ROOT/services/webapp/target/xowl-service-webapp-$VERSION.jar" "$1"

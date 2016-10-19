99c10e10fc39f00fac8787a2036f3614d78234cb
Laurent Wouters <lwouters@cenotelie.fr>
2016-10-19 14:36:40 +0200

    [releng] Fixed deployment script

--- .releng/deploy-bundles.sh
+++ .releng/deploy-bundles.sh
@@ -8,8 +8,6 @@ VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -f "$ROO
 
 cp "$ROOT/kernel/target/dependency/redist-"* "$1"
 cp "$ROOT/kernel/target/dependency/xowl-"* "$1"
-cp "$ROOT/kernel/target/dependency/shiro-"* "$1"
-cp "$ROOT/kernel/target/dependency/slf4j-"* "$1"
 cp "$ROOT/services/lts/target/dependency/xowl-"* "$1"
 cp "$ROOT/kernel/target/xowl-kernel-$VERSION.jar" "$1"
 cp "$ROOT/services/connection/target/xowl-service-connection-$VERSION.jar" "$1"
@@ -21,6 +19,8 @@ cp "$ROOT/services/importation/target/xowl-service-importation-$VERSION.jar" "$1
 cp "$ROOT/services/lts/target/xowl-service-lts-$VERSION.jar" "$1"
 cp "$ROOT/services/security-internal/target/xowl-service-security-internal-$VERSION.jar" "$1"
 cp "$ROOT/services/security-shiro/target/xowl-service-security-shiro-$VERSION.jar" "$1"
+cp "$ROOT/services/security-shiro/target/dependency/shiro-"* "$1"
+cp "$ROOT/services/security-shiro/target/dependency/slf4j-"* "$1"
 cp "$ROOT/services/webapp/target/xowl-service-webapp-$VERSION.jar" "$1"
 cp "$ROOT/connectors/csv/target/xowl-connector-csv-$VERSION.jar" "$1"
 cp "$ROOT/connectors/doors9/target/xowl-connector-doors9-$VERSION.jar" "$1"

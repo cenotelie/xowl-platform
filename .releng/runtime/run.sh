9ed1693d7b56cf81aaad232e865ac8fa68b3dead
Laurent Wouters <lwouters@cenotelie.fr>
2016-11-03 16:49:09 +0100

    [feature] Support platform restart and shutdown

--- .releng/runtime/run.sh
+++ .releng/runtime/run.sh
@@ -3,4 +3,10 @@
 SCRIPT="$(readlink -f "$0")"
 ROOT="$(dirname "$SCRIPT")"
 
-java "-Dxowl.conf.dir=$ROOT/config" "-Dxowl.root=$ROOT/felix" -jar "$ROOT/felix/bin/felix.jar" -b "$ROOT/felix/bundle"
+CODE=5
+while [ "$CODE" -eq 5 ]
+  do
+    java "-Dxowl.conf.dir=$ROOT/config" "-Dxowl.root=$ROOT/felix" -jar "$ROOT/felix/bin/felix.jar" -b "$ROOT/felix/bundle"
+    CODE=$?
+    echo "Exit code is $CODE"
+done

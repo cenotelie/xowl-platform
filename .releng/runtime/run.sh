c70e6f8c64068171e3a35fa0488dc2626f60e131
Laurent Wouters <lwouters@cenotelie.fr>
2016-11-03 16:28:20 +0100

    [feature] Enabling runtime TSL certificate generation

--- .releng/runtime/run.sh
+++ .releng/runtime/run.sh
@@ -3,4 +3,4 @@
 SCRIPT="$(readlink -f "$0")"
 ROOT="$(dirname "$SCRIPT")"
 
-java "-Dxowl.conf.dir=$ROOT/config" "-Dxowl.root=$ROOT" -jar "$ROOT/felix/bin/felix.jar" -b "$ROOT/felix/bundle"
+java "-Dxowl.conf.dir=$ROOT/config" "-Dxowl.root=$ROOT/felix" -jar "$ROOT/felix/bin/felix.jar" -b "$ROOT/felix/bundle"

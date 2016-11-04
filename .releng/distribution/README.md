b1130da5285246ded5a50119aff6a7f8c517cf7c
Laurent Wouters <lwouters@cenotelie.fr>
2016-11-04 10:25:01 +0100

    [releng] Refactoring the release engineering

---
+++ .releng/distribution/README.md
@@ -0,0 +1,17 @@
+# Distribution
+
+A distribution of the xOWL Federation Platform is structured as follow:
+
+* Distribution root's directory
+    * `config` The directory for the platform's configuration
+    * `felix` The felix OSGi platform
+        * `felix/bin` The main executable for Felix
+        * `felix/bundle` The directory for the platform's bundles
+        * `felix/conf` The configuration for Felix
+    * `do-run.sh` The script to run a single instance of the platform
+    * `admin.sh` In the standalone distribution, the Linux service management script
+    * `install-daemon.sh` In the standalone distribution, the Linux service installation
+    * `uninstall-daemon.sh` In the standalone distribution, the Linux service un-installation script
+
+Variables bound at runtime:
+* `xowl.root` The distribution root's directory
--- .releng/distribution/do-run.sh
+++ .releng/distribution/do-run.sh
@@ -2,12 +2,9 @@
 
 # Runs an instance of the xOWL platform
 # This scripts looks for a special return value to indicate that the server requested a restart
-# Expected parameter:
-#   1: Path to the configuration directory
 
 SCRIPT="$(readlink -f "$0")"
 DISTRIB="$(dirname "$SCRIPT")"
-CONFIG=$1
 
 # custom path to java, if any
 # if left empty, the script will use 'which' to locate java
@@ -23,17 +20,11 @@ if [ -z $JAVA ]
     exit 1
 fi
 
-if [ -z $CONFIG ]
-  then
-    # No configuration directory provided, uses the distribution directory
-    CONFIG="$DISTRIB/config"
-fi
-
 # while exit code is 5 (restart) relaunch the process
 CODE=5
 while [ "$CODE" -eq 5 ]
   do
-    $JAVA "-Dxowl.conf.dir=$CONFIG" "-Dxowl.root=$DISTRIB/felix" -jar "$DISTRIB/felix/bin/felix.jar" -b "$DISTRIB/felix/bundle"
+    $JAVA "-Dxowl.root=$DISTRIB" -jar "$DISTRIB/felix/bin/felix.jar" -b "$DISTRIB/felix/bundle"
     CODE=$?
     echo "Exit code is $CODE"
 done

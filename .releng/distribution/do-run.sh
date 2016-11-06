3125585f31ba17c7b3586bb58ccd7733c3c21a71
Laurent Wouters <lwouters@cenotelie.fr>
2016-11-06 16:11:09 +0100

    [releng] Clear Felix cache on startup to enable the re-deployment of bundles

--- .releng/distribution/do-run.sh
+++ .releng/distribution/do-run.sh
@@ -24,6 +24,7 @@ fi
 CODE=5
 while [ "$CODE" -eq 5 ]
   do
+    rm -rf "$DISTRIB/felix-cache"
     $JAVA "-Dgosh.args=--noi" "-Dxowl.root=$DISTRIB" -jar "$DISTRIB/felix/bin/felix.jar" -b "$DISTRIB/felix/bundle"
     CODE=$?
     echo "Exit code is $CODE"

501359bda39e5b8f3be1775b4790e55c290f897b
Laurent Wouters <lwouters@cenotelie.fr>
2016-11-08 21:15:40 +0100

    [fix] Replacing the semantic web dataset connector and importer

--- connectors/semanticweb/src/main/resources/org/xowl/platform/connectors/semanticweb/wizard.js
+++ connectors/semanticweb/src/main/resources/org/xowl/platform/connectors/semanticweb/wizard.js
@@ -17,6 +17,13 @@ function init() {
 			{name: "Semantic Web Importer"}], function() {
 		if (!docId || docId === null || docId === "")
 			return;
+		var typesField = document.getElementById("input-syntax");
+		for (var i = 0; i != MIME_TYPES.length; i++) {
+			var option = document.createElement("option");
+			option.value = MIME_TYPES[i].value;
+			option.appendChild(document.createTextNode(MIME_TYPES[i].name));
+			typesField.appendChild(option);
+		}
 		doGetDocument();
 	});
 }
@@ -27,6 +34,51 @@ function doGetDocument() {
 	xowl.getUploadedDocument(function (status, ct, content) {
 		if (onOperationEnded(status, content)) {
 			DOCUMENT = content;
+			document.getElementById("document-name").value = DOCUMENT.name;
+			var fileType = null;
+			for (var i = 0; i != MIME_TYPES.length; i++) {
+				for (var j = 0; j != MIME_TYPES[i].extensions.length; j++) {
+					var suffix = MIME_TYPES[i].extensions[j];
+					if (DOCUMENT.fileName.indexOf(suffix, DOCUMENT.fileName.length - suffix.length) !== -1) {
+						fileType = MIME_TYPES[i];
+						break;
+					}
+				}
+			}
+			if (fileType !== null) {
+				document.getElementById("input-syntax").value = fileType.value;
+			}
 		}
 	}, docId);
+}
+
+function onImport() {
+	if (!onOperationRequest({ type: "org.xowl.platform.kernel.RichString", parts: ["Importing document ", DOCUMENT, " ..."]}))
+		return;
+	xowl.importUploadedDocument(function (status, ct, content) {
+		if (onOperationEnded(status, content)) {
+			displayMessage("success", { type: "org.xowl.platform.kernel.RichString", parts: ["Launched importation job for ", DOCUMENT, "."]});
+			waitForJob(content.identifier, content.name, function (job) {
+				onJobCompleted(job);
+			});
+		}
+	}, docId, importerId, {
+		family: base,
+		version: version,
+		archetype: archetype,
+		superseded: [],
+		syntax: document.getElementById("input-syntax").value
+	});
+}
+
+function onJobCompleted(job) {
+	if (!job.result.hasOwnProperty("isSuccess")) {
+		displayMessage("error", "No result ...");
+	} else if (!job.result.isSuccess) {
+		displayMessage("error", "FAILURE: " + job.result.message);
+	} else {
+		var artifactId = job.result.payload;
+		displayMessage("success", { type: "org.xowl.platform.kernel.RichString", parts: ["Imported ", DOCUMENT, " as artifact " + artifactId]});
+		waitAndGo("/web/modules/core/artifacts/artifact.html?id=" + encodeURIComponent(artifactId));
+	}
 }

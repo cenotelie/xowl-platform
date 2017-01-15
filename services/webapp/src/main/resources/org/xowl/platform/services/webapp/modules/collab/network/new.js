4f09edcbd98bdb6fc794c83090ee61fc00e00811
Laurent Wouters <lwouters@cenotelie.fr>
2017-01-15 14:22:07 +0100

    [feature] Implementation of the core collaboration API

--- services/webapp/src/main/resources/org/xowl/platform/services/webapp/modules/collab/network/new.js
+++ services/webapp/src/main/resources/org/xowl/platform/services/webapp/modules/collab/network/new.js
@@ -26,6 +26,17 @@ function init() {
 		setupAutocompleteSpecification("output-specification");
 		setupAutocompleteRole("role-existing");
 		setupAutocompletePattern("collaboration-pattern");
+		doGetData();
+	});
+}
+
+function doGetData() {
+	if (!onOperationRequest("Loading ..."))
+		return;
+	xowl.getArtifactArchetypes(function (status, ct, content) {
+		if (onOperationEnded(status, content)) {
+			platformArchetypes = content;
+		}
 	});
 }
 
@@ -42,16 +53,7 @@ function filterItems(items, value) {
 function setupAutocompleteArchetype(component) {
 	var autocomplete = new AutoComplete(component);
 	autocomplete.lookupItems = function (value) {
-		if (platformArchetypes !== null) {
-			autocomplete.onItems(filterItems(platformArchetypes, value));
-			return;
-		}
-		xowl.getArtifactArchetypes(function (status, ct, content) {
-			if (status === 200) {
-				platformArchetypes = content;
-				autocomplete.onItems(filterItems(platformArchetypes, value));
-			}
-		});
+		autocomplete.onItems(filterItems(platformArchetypes, value));
 	};
 	autocomplete.renderItem = function (item) {
 		var result = document.createElement("div");
@@ -149,23 +151,217 @@ function onClickCreateInput() {
 		name: name,
 		archetype: archetype
 	};
-	specification.inputs.push(input);
+	doAddInput(input);
+	document.getElementById("input-identifier").value = "";
+	document.getElementById("input-name").value = "";
+	document.getElementById("input-archetype").value = "";
 }
 
 function onClickAddInput() {
+	var identifier = document.getElementById("input-specification").value;
+	if (identifier == null || identifier == "")
+		return;
+	var input = null;
+	for (var i = 0; i != platformSpecifications.length; i++) {
+		if (platformSpecifications[i].identifier == identifier) {
+			input = platformSpecifications[i];
+			break;
+		}
+	}
+	if (input == null)
+		return;
+	doAddInput(input);
+	document.getElementById("input-specification").value = "";
+}
+
+function doAddInput(input) {
+	specification.inputs.push(input);
+	var table = document.getElementById("inputs");
+	var row = null;
+	var toRemove = function () {
+		table.removeChild(row);
+		specification.inputs.splice(specification.inputs.indexOf(input), 1);
+	};
+	row = renderSpecification(input, toRemove);
+	table.appendChild(row);
 }
 
 function onClickCreateOutput() {
+	var identifier = document.getElementById("output-identifier").value;
+	var name = document.getElementById("output-name").value;
+	var archetype = document.getElementById("output-archetype").value;
+	if (identifier == null || identifier == ""
+		|| name == null || name == ""
+		|| archetype == null || archetype == "") {
+		return;
+	}
+	var output = {
+		identifier: identifier,
+		name: name,
+		archetype: archetype
+	};
+	doAddOutput(output);
+	document.getElementById("output-identifier").value = "";
+	document.getElementById("output-name").value = "";
+	document.getElementById("output-archetype").value = "";
 }
 
 function onClickAddOutput() {
+	var identifier = document.getElementById("output-specification").value;
+	if (identifier == null || identifier == "")
+		return;
+	var output = null;
+	for (var i = 0; i != platformSpecifications.length; i++) {
+		if (platformSpecifications[i].identifier == identifier) {
+			output = platformSpecifications[i];
+			break;
+		}
+	}
+	if (output == null)
+		return;
+	doAddOutput(output);
+	document.getElementById("output-specification").value = "";
+}
+
+function doAddOutput(output) {
+	specification.outputs.push(output);
+	var table = document.getElementById("outputs");
+	var row = null;
+	var toRemove = function () {
+		table.removeChild(row);
+		specification.outputs.splice(specification.outputs.indexOf(output), 1);
+	};
+	row = renderSpecification(output, toRemove);
+	table.appendChild(row);
 }
 
 function onClickCreateRole() {
+	var identifier = document.getElementById("role-identifier").value;
+	var name = document.getElementById("role-name").value;
+	if (identifier == null || identifier == ""
+		|| name == null || name == "") {
+		return;
+	}
+	var role = {
+		identifier: identifier,
+		name: name
+	};
+	doAddRole(role);
+	document.getElementById("role-identifier").value = "";
+	document.getElementById("role-name").value = "";
 }
 
 function onClickAddRole() {
+	var identifier = document.getElementById("role-existing").value;
+	if (identifier == null || identifier == "")
+		return;
+	var role = null;
+	for (var i = 0; i != platformRoles.length; i++) {
+		if (platformRoles[i].identifier == identifier) {
+			role = platformRoles[i];
+			break;
+		}
+	}
+	if (role == null)
+		return;
+	doAddRole(role);
+	document.getElementById("role-existing").value = "";
+}
+
+function doAddRole(role) {
+	specification.roles.push(role);
+	var table = document.getElementById("roles");
+	var row = null;
+	var toRemove = function () {
+		table.removeChild(row);
+		specification.roles.splice(specification.roles.indexOf(role), 1);
+	};
+	row = renderRole(role, toRemove);
+	table.appendChild(row);
 }
 
 function onClickSpawn() {
+}
+
+
+
+
+function renderSpecification(specification, toRemove) {
+	var row = document.createElement("tr");
+	var cell = document.createElement("td");
+	var image = document.createElement("img");
+	image.src = "/web/assets/specification.svg";
+	image.width = 30;
+	image.height = 30;
+	image.style.marginRight = "20px";
+	image.title = specification.identifier;
+	var span = document.createElement("span");
+	span.appendChild(document.createTextNode(specification.name));
+	span.title = specification.identifier;
+	cell.appendChild(image);
+	cell.appendChild(span);
+	row.appendChild(cell);
+
+	cell = document.createElement("td");
+	cell.appendChild(renderArchetype(specification.archetype));
+	row.appendChild(cell);
+
+	cell = document.createElement("td");
+	image = document.createElement("img");
+	image.src = "/web/assets/action-remove.svg";
+	image.width = 20;
+	image.height = 20;
+	image.title = "REMOVE";
+	var button = document.createElement("span");
+	button.classList.add("btn");
+	button.classList.add("btn-default");
+	button.appendChild(image);
+	button.onclick = toRemove;
+	cell.appendChild(button);
+	row.appendChild(cell);
+	return row;
+}
+
+function renderArchetype(archetypeId) {
+	for (var i = 0; i != platformArchetypes.length; i++) {
+		if (platformArchetypes[i].identifier == archetypeId) {
+			var span = document.createElement("span");
+			span.appendChild(document.createTextNode(platformArchetypes[i].name));
+			span.title = platformArchetypes[i].identifier;
+			return span;
+		}
+	}
+	return document.createTextNode(archetypeId);
+}
+
+function renderRole(role, toRemove) {
+	var row = document.createElement("tr");
+	var cell = document.createElement("td");
+	var image = document.createElement("img");
+	image.src = "/web/assets/role.svg";
+	image.width = 30;
+	image.height = 30;
+	image.style.marginRight = "20px";
+	image.title = role.identifier;
+	var span = document.createElement("span");
+	span.appendChild(document.createTextNode(role.name));
+	span.title = role.identifier;
+	cell.appendChild(image);
+	cell.appendChild(span);
+	row.appendChild(cell);
+
+	cell = document.createElement("td");
+	image = document.createElement("img");
+	image.src = "/web/assets/action-remove.svg";
+	image.width = 20;
+	image.height = 20;
+	image.title = "REMOVE";
+	var button = document.createElement("span");
+	button.classList.add("btn");
+	button.classList.add("btn-default");
+	button.appendChild(image);
+	button.onclick = toRemove;
+	cell.appendChild(button);
+	row.appendChild(cell);
+	return row;
 }

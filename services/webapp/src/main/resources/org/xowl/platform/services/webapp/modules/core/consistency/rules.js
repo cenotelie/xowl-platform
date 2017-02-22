efa1f2e11c60311dff2222da598570696791101c
Laurent Wouters <lwouters@cenotelie.fr>
2017-02-22 10:45:46 +0100

    [clean] Cleanup web app

--- services/webapp/src/main/resources/org/xowl/platform/services/webapp/modules/core/consistency/rules.js
+++ services/webapp/src/main/resources/org/xowl/platform/services/webapp/modules/core/consistency/rules.js
@@ -29,8 +29,11 @@ function renderRules(rules) {
 }
 
 function renderRule(rule, index) {
-	var cell1 = document.createElement("td");
-	cell1.appendChild(document.createTextNode((index + 1).toString()));
+	var row = document.createElement("tr");
+	var cell = document.createElement("td");
+	cell.appendChild(document.createTextNode((index + 1).toString()));
+	row.appendChild(cell);
+
 	var icon = document.createElement("img");
 	icon.src = ROOT + "/assets/rule.svg";
 	icon.width = 40;
@@ -40,35 +43,46 @@ function renderRule(rule, index) {
 	var link = document.createElement("a");
 	link.appendChild(document.createTextNode(rule.name));
 	link.href = "rule.html?id=" + encodeURIComponent(rule.identifier);
-	var cell2 = document.createElement("td");
-	cell2.appendChild(icon);
-	cell2.appendChild(link);
+	cell = document.createElement("td");
+	cell.appendChild(icon);
+	cell.appendChild(link);
+	row.appendChild(cell);
+
 	var toggle = document.createElement("div");
 	toggle.appendChild(document.createElement("button"));
 	toggle.classList.add("toggle-button");
 	if (rule.isActive)
 		toggle.classList.add("toggle-button-selected");
 	toggle.onclick = function (evt) { onToggleRule(toggle, rule); };
-	var cell3 = document.createElement("td");
-	cell3.appendChild(toggle);
-	var span = document.createElement("span");
-	span.classList.add("glyphicon");
-	span.classList.add("glyphicon-minus");
-	span.setAttribute("aria-hidden", "true");
-	var button = document.createElement("a");
+	cell = document.createElement("td");
+	cell.appendChild(toggle);
+	row.appendChild(cell);
+
+	cell = document.createElement("td");
+	image = document.createElement("img");
+	image.src = ROOT + "/assets/action-remove.svg";
+	image.width = 20;
+	image.height = 20;
+	image.title = "DELETE";
+	var button = document.createElement("span");
 	button.classList.add("btn");
-	button.classList.add("btn-xs");
-	button.classList.add("btn-danger");
-	button.title = "DELETE";
-	button.appendChild(span);
+	button.classList.add("btn-default");
+	button.style.marginRight = "20px";
+	button.appendChild(image);
 	button.onclick = function (evt) { onDeleteRule(rule); };
-	var cell4 = document.createElement("td");
-	cell4.appendChild(button);
-	var row = document.createElement("tr");
-	row.appendChild(cell1);
-	row.appendChild(cell2);
-	row.appendChild(cell3);
-	row.appendChild(cell4);
+	cell.appendChild(button);
+	image = document.createElement("img");
+	image.src = ROOT + "/assets/action-download.svg";
+	image.width = 20;
+	image.height = 20;
+	image.title = "DOWNLOAD";
+	button = document.createElement("span");
+	button.classList.add("btn");
+	button.classList.add("btn-default");
+	button.appendChild(image);
+	button.onclick = function (evt) { onDownloadDefinition(rule); };
+	cell.appendChild(button);
+	row.appendChild(cell);
 	return row;
 }
 
@@ -123,4 +137,10 @@ function onDeleteRule(rule) {
 			waitAndRefresh();
 		}
 	}, rule.identifier);
+}
+
+function onDownloadDefinition(rule) {
+}
+
+function onClickDownload() {
 }

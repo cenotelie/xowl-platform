8f69094e4b49bd0f180360e7f444727ce4d7bff3
Laurent Wouters <lwouters@cenotelie.fr>
2017-03-11 18:49:36 +0100

    [feature] Implementation of bot service

--- services/webapp/src/main/resources/org/xowl/platform/services/webapp/modules/admin/bots/bot.js
+++ services/webapp/src/main/resources/org/xowl/platform/services/webapp/modules/admin/bots/bot.js
@@ -12,24 +12,55 @@ function init() {
 			{name: "Bot " + botId}], function() {
 		if (!botId || botId === null || botId === "")
 			return;
-		if (!onOperationRequest("Loading ..."))
-			return;
-		xowl.getBot(function (status, ct, content) {
-			if (onOperationEnded(status, content)) {
-				bot = content;
-				render();
-			}
-		}, botId);
+		doGetData();
 	});
 }
 
-function render() {
+function doGetData() {
+	if (!onOperationRequest("Loading ...", 2))
+		return;
+	xowl.getBot(function (status, ct, content) {
+		if (onOperationEnded(status, content)) {
+			bot = content;
+			renderBotData();
+		}
+	}, botId);
+	xowl.getBotMessages(function (status, ct, content) {
+		if (onOperationEnded(status, content)) {
+			bot = content;
+			renderBotMessages();
+		}
+	}, botId);
+}
+
+function renderBotData() {
 	document.getElementById("bot-identifier").value = bot.identifier;
 	document.getElementById("bot-name").value = bot.name;
 	document.getElementById("bot-type").value = bot.botType;
 	document.getElementById("bot-wakeup-on-startup").value = (bot.wakeupOnStartup ? "YES" : "NO");
 	document.getElementById("bot-security-user").value = bot.securityUser;
 	document.getElementById("bot-status").value = bot.status;
+}
+
+function renderBotMessages(messages) {
+	var table = document.getElementById("messages");
+	for (var  i = 0; i != messages.length; i++) {
+		table.appendChild(renderBotMessage(messages[i]));
+	}
+}
+
+function renderBotMessage(message) {
+	var row = document.createElement("tr");
+	var cells = [document.createElement("td"),
+		document.createElement("td"),
+		document.createElement("td")];
+	cells[0].appendChild(document.createTextNode(message.level));
+	cells[1].appendChild(document.createTextNode(message.date));
+	cells[2].appendChild(renderMessage(message.content));
+	row.appendChild(cells[0]);
+	row.appendChild(cells[1]);
+	row.appendChild(cells[2]);
+	return row;
 }
 
 function doWakeup() {

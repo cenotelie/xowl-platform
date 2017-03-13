2cccbfb887da13ceed92018962e8f2e198b3a809
Laurent Wouters <lwouters@cenotelie.fr>
2017-03-13 13:36:17 +0100

    [feature] Implementation of user profiles

--- services/webapp/src/main/resources/org/xowl/platform/services/webapp/modules/collab/community/profile-view.js
+++ services/webapp/src/main/resources/org/xowl/platform/services/webapp/modules/collab/community/profile-view.js
@@ -11,6 +11,8 @@ function init() {
 			{name: "Profile " + profileId}], function() {
 		if (!profileId || profileId === null || profileId === "")
 			return;
+		document.getElementById("profile-edit-link").href = "profile-update.html?id=" + encodeURIComponent(profileId);
+		document.getElementById("profile-security-link").href = ROOT + "/modules/admin/security/user.html?id=" + encodeURIComponent(profileId);
 		doGetData();
 	});
 }
@@ -19,11 +21,81 @@ function doGetData() {
 	if (!onOperationRequest("Loading ...", 2))
 		return;
 	xowl.getPlatformUser(function (status, ct, content) {
-		if (onOperationEnded(status, content, null)) {
+		if (onOperationEnded(status, content)) {
+			renderUser(content);
 		}
 	}, profileId);
 	xowl.getPublicProfile(function (status, ct, content) {
 		if (onOperationEnded(status, content, null)) {
+			renderProfile(content);
+		} else if (status === 404) {
+			document.getElementById("row-new-profile").style.display = "";
+			document.getElementById("profile-new-link").href= "profile-update.html?id=" + encodeURIComponent(profileId);
 		}
 	}, profileId);
+}
+
+function renderUser(user) {
+	document.getElementById("profile-identifier").value = user.identifier;
+	document.getElementById("profile-name").value = user.name;
+	var panel = document.getElementById("panel-roles");
+	for (var i = 0; i != user.roles.length; i++) {
+		panel.appendChild(renderRole(user.roles[i]));
+	}
+}
+
+function renderRole(role) {
+	var cell = document.createElement("span");
+	cell.style.marginLeft = "5px";
+	cell.style.marginRight = "5px";
+	cell.style.marginTop = "5px";
+	cell.style.marginBottom = "5px";
+	var image = document.createElement("img");
+	image.src = ROOT + "/assets/role.svg";
+	image.width = 30;
+	image.height = 30;
+	image.style.marginRight = "20px";
+	image.title = role.identifier;
+	var link = document.createElement("a");
+	link.appendChild(document.createTextNode(role.name));
+	link.href = ROOT + "/modules/admin/security/role.html?id=" + encodeURIComponent(role.identifier);
+	cell.appendChild(image);
+	cell.appendChild(link);
+	return cell;
+}
+
+function renderProfile(profile) {
+	if (profile.avatarContent.length > 0) {
+		document.getElementById("avatar").src = "data:" + profile.avatarMime + ";base64," + profile.avatarContent;
+	}
+	if (profile.email.length > 0) {
+		document.getElementById("profile-email").appendChild(document.createTextNode(profile.email));
+		document.getElementById("profile-email").href = "mailto:" + encodeURIComponent(profile.email);
+	}
+	document.getElementById("profile-organization").value = profile.organization;
+	document.getElementById("profile-occupation").value = profile.occupation;
+	var panel = document.getElementById("panel-badges");
+	for (var i = 0; i != profile.badges.length; i++) {
+		panel.appendChild(renderBadge(profile.badges[i]));
+	}
+}
+
+function renderBadge(badge) {
+	var cell = document.createElement("span");
+	cell.style.marginLeft = "5px";
+	cell.style.marginRight = "5px";
+	cell.style.marginTop = "5px";
+	cell.style.marginBottom = "5px";
+	var image = document.createElement("img");
+	image.src = "data:" + badge.imageMime + ";base64," + badge.imageContent;
+	image.width = 30;
+	image.height = 30;
+	image.style.marginRight = "20px";
+	image.title = badge.identifier;
+	var link = document.createElement("a");
+	link.appendChild(document.createTextNode(badge.name));
+	link.title = badge.description;
+	cell.appendChild(image);
+	cell.appendChild(link);
+	return cell;
 }

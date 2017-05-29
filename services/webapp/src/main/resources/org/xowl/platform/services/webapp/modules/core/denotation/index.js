7b1c5c2809deea23dfe3d5311ec8634613b87e1e
Laurent Wouters <lwouters@cenotelie.fr>
2017-05-29 23:25:34 +0200

    [fix] Added skeleton for empty services

---
+++ services/webapp/src/main/resources/org/xowl/platform/services/webapp/modules/core/denotation/index.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2016 Association Cénotélie (cenotelie.fr)
+// Provided under LGPL v3
+
+var xowl = new XOWL();
+
+function init() {
+	doSetupPage(xowl, true, [
+			{name: "Core Services", uri: ROOT + "/modules/core/"},
+			{name: "Share Meaning"}], function() {
+	});
+}

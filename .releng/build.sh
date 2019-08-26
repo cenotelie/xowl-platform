66c4c4334b46cc2b22ffbe9a19df1843953c15a6
Laurent Wouters <lwouters@cenotelie.fr>
2019-08-26 10:41:00 +0200

    [releng] Add azure pipeline

--- .releng/build.sh
+++ .releng/build.sh
@@ -4,23 +4,9 @@ SCRIPT="$(readlink -f "$0")"
 RELENG="$(dirname "$SCRIPT")"
 ROOT="$(dirname "$RELENG")"
 
-while [ $# -gt 0 ]; do
-  case "$1" in
-    --repository=*)
-      REPOSITORY="${1#*=}"
-      ;;
-    *)
-      printf "***************************\n"
-      printf "* Error: Invalid argument.*\n"
-      printf "***************************\n"
-      exit 1
-  esac
-  shift
-done
-
 # Extract the product
 VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -f "$ROOT/pom.xml" -Dexpression=project.version|grep -Ev '(^\[|Download\w+:)')
-HASH=$(hg -R "$ROOT" --debug id -i)
+HASH=$(git rev-parse HEAD)
 echo "Building artifacts for version $VERSION ($HASH)"
 tar -C "$RELENG/docker" -xf "$ROOT/distribution/target/xowl-distribution-$VERSION.tar.gz"
 
@@ -30,14 +16,3 @@ docker build --tag "xowl/xowl-platform:$VERSION" --rm --label version="$VERSION"
 
 # Cleanup
 rm -rf "$RELENG/docker/xowl-distribution"
-
-# Publish
-if [ ! -z "$REPOSITORY" ]; then
-  docker tag  "xowl/xowl-platform:$VERSION" "$REPOSITORY/xowl/xowl-platform:$VERSION"
-  docker tag  "$REPOSITORY/xowl/xowl-platform:$VERSION" "$REPOSITORY/xowl/xowl-platform:latest"
-  docker push "$REPOSITORY/xowl/xowl-platform:$VERSION"
-  docker push "$REPOSITORY/xowl/xowl-platform:latest"
-  docker rmi  "$REPOSITORY/xowl/xowl-platform:$VERSION"
-  docker rmi  "$REPOSITORY/xowl/xowl-platform:latest"
-  docker rmi  "xowl/xowl-platform:$VERSION"
-fi
